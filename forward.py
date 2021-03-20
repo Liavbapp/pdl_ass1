2ee9899510414789ba972c9340d1c8d845bdc3d6
Liav <liavbapp@gmail.com>
2021-03-20 20:10:06 +0200

    implemented the softmax loss

--- forward.py
+++ forward.py
@@ -1,50 +1,49 @@
 import numpy as np
 
-#
-# def softmax(X, W, j, num_classes):
-#     """
-#     :param num_classes: number of classification class
-#     :param j: current classification class
-#     :param X: data matrix - dimension: n x m
-#     :param W: weights matrix : dimension: n x l
-#     :return prob: probability that X belongs to class j
-#     """
-#     X_t = np.transpose(X)
-#     numerator = np.exp(np.matmul(X_t, W[:, j]))
-#     prob = numerator / denominator
-#     return prob
-#
-#
-# def safe_softmax(X, W, j, num_classes, eta):
-#     """
-#     :param eta: max(X_t*w_i for i in {0,1,... num_classes})
-#     :param num_classes: number of classification class
-#     :param j: current classification class
-#     :param X: matrix of input data - dimension: n x m
-#     :param W: matrix of weights vectors : dimension: n x l
-#     :return prob: probability that X belongs to class j
-#     """
-#     X_t = np.transpose(X)
-#     numerator = np.exp(np.matmul(X_t, W[:, j]) - eta)
-#     prob = numerator / softmax_denominator
-#     return prob
-#
 
-def cross_entropy_softmax_lost(X, C, W, with_eta=False):
+def compute_softmax_gradient_vector(X, W, C, b):
+    """
+    :param X: data matrix - dimension: n x m
+    :param W: weights matrix - dimension: n x l
+    :param C: classes vector matrix - dimension: l x m
+    :param b: bias vector - length: l
+    :return:
+    """
+    m = len(X[0])
+    l = len(C)
+    W_t = np.transpose(W)
+    b_t = np.expand_dims(b, 1)
+    nominator = np.exp(np.matmul(W_t, X) + b_t)
+    dominator = sum(np.exp(np.matmul(np.transpose(W[:, k]), X) + b[k]) for k in range(0, l))
+    grads = (1 / m) * np.matmul(W, (nominator / dominator) - C)
+    return grads
+
+
+def cross_entropy_softmax_lost(X, C, W, b, with_eta=False):
     """
     computation of lost function
+    :param with_eta: use eta for safety
     :param X: data matrix - dimension: n x m
-    :param C: classes vector matrix - dimension: m x l
+    :param C: classes vector matrix - dimension: l x m
     :param W: weights matrix - dimension: n x l
+    :param b: bias vector - length: l
     :param num_classes:
     :return:
     """
-    l = len(C[0])  # why l?
+    l = len(C)
     m = len(X[0])
     X_t = np.transpose(X)
-    eta = max([np.matmul(X_t, W[:, i]) for i in range(0, l)]) if with_eta else 0
-    softmax_denominator = np.sum(np.exp(np.matmul(X_t, W[:, j]) - eta) for j in range(0, l))
-    loss = (-1 / m) * np.sum(np.matmul(np.transpose(C[:, k]), np.log(np.exp(np.matmul(X_t, W[:, k])))) for k in range(0, l))  # should deviate
-    return loss
+    eta_lst = [max([np.matmul(X_t[i], W[:, k]) + b[k] for k in range(0, l)]) for i in range(0, m)] if with_eta else [
+                                                                                                                        0] * m
+    softmax_denominator = sum([np.exp(np.matmul(X_t, W[:, k]) + b[k] - eta_lst) for k in range(0, l)])
+    loss = 0
+    for k in range(0, l):
+        softmax_prob = np.log(np.exp(np.matmul(X_t, W[:, k]) + b[k] - eta_lst) / softmax_denominator)
+        loss += np.matmul(C[k], softmax_prob)
+    loss *= (-1 / m)
 
-cross_entropy_softmax_lost(X, C, W, with_eta=False)
+    # loss = (-1 / m) * sum(
+    #     np.matmul(C[k], np.log(np.exp(np.matmul(X_t, W[:, k]) + b[k] - eta_lst) / softmax_denominator)) for k
+    #     in range(0, l))
+
+    return loss
--- tests.py
+++ tests.py
@@ -1,16 +1,17 @@
 import unittest
 import numpy as np
 
+import forward
 from forward import cross_entropy_softmax_lost
 
 
 class Tests(unittest.TestCase):
 
-    def test_cross_entropy_softmax_lost(self):
+    def test_cross_entropy_softmax_lost_no_eta(self):
         """
         computation of lost function
         :param X: data matrix - dimension: n x m
-        :param C: classes vector matrix - dimension: m x l
+        :param C: classes vector matrix - dimension: l x m
         :param W: weights matrix - dimension: n x l
         :param num_classes:
         :return:
@@ -18,19 +19,63 @@ class Tests(unittest.TestCase):
         X = np.array([[1, 2, 3],
                       [3, 4, 4]])
 
-        C = np.array([[0, 1],
-                      [1, 0],
-                      [0, 1]])
+        C = np.array([[0, 1, 0],
+                     [1, 0, 1]])
 
-        W = np.array([[1, 2],
-                      [2, 1]])
+        W = np.array([[1, 3],
+                      [5, 1]])
 
-        l = len(C[0])  # why l?
-        m = len(X[0])
-        X_t = np.transpose(X)
-        eta = max([np.matmul(X_t[i], W[:, i]) for i in range(0, l)]) if with_eta else 0
-        softmax_denominator = np.sum(np.exp(np.matmul(X_t, W[:, j]) - eta) for j in range(0, l))
-        loss = (-1 / m) * np.sum(np.matmul(np.transpose(C[:, k]), np.log(np.exp(np.matmul(X_t, W[:, k])))) for k in range(0, l))  # should deviate
-        return loss
+        b = np.array([0, 0])
 
-        return cross_entropy_softmax_lost(X, C, W)
+        expected_loss = 6.666698980663971
+        actual_loss = forward.cross_entropy_softmax_lost(X, C, W, b, with_eta=False)
+        self.assertTrue(expected_loss == actual_loss)
+
+    def test_cross_entropy_softmax_lost_with_eta(self):
+        """
+        computation of lost function
+        :param X: data matrix - dimension: n x m
+        :param C: classes vector matrix - dimension: l x m
+        :param W: weights matrix - dimension: n x l
+        :param num_classes:
+        :return:
+        """
+        X = np.array([[1, 2, 3],
+                      [3, 4, 4]])
+
+        C = np.array([[0, 1, 0],
+                      [1, 0, 1]])
+
+        W = np.array([[1, 3],
+                      [5, 1]])
+
+        b = np.array([0, 0])
+
+        expected_loss = 6.666698980663971
+        actual_loss = forward.cross_entropy_softmax_lost(X, C, W, b, with_eta=True)
+        self.assertTrue(expected_loss == actual_loss)
+
+    def test_cross_entropy_softmax_lost_with_bias(self):
+        """
+        computation of lost function
+        :param X: data matrix - dimension: n x m
+        :param C: classes vector matrix - dimension: l x m
+        :param W: weights matrix - dimension: n x l
+        :param num_classes:
+        :return:
+        """
+        X = np.array([[1, 2, 3],
+                      [3, 4, 4]])
+
+        C = np.array([[0, 1, 0],
+                      [1, 0, 1]])
+
+        W = np.array([[1, 3],
+                      [5, 1]])
+
+        b = np.array([1, 4])
+
+        expected_loss = 4.667315445032424
+        actual_loss = forward.cross_entropy_softmax_lost(X, C, W, b, with_eta=True)
+        self.assertTrue(expected_loss == actual_loss)
+
