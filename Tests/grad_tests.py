86a2451a4233733f51353203a3f280b75fabf7e7
Guy Har Zahav <guyharzahav43@gmail.com>
2021-04-05 19:59:05 +0300

    test JTMV

--- Tests/grad_tests.py
+++ Tests/grad_tests.py
@@ -3,6 +3,7 @@ import numpy as np
 from numpy import linalg
 
 import Components.forward as forward
+from Components import backward
 
 
 class GradTests(unittest.TestCase):
@@ -50,6 +51,55 @@ class GradTests(unittest.TestCase):
         factors_4 = [res_sum2[i] / res_sum2[i + 1] for i in range(0, len(res_sum2) - 1)]
         print(f'factors 2: {factors_2}')
         print(f'factors 4: {factors_4}')
+
+
+
+    def test_grad_for_JTMV_X(self):
+        """
+        :param X: data matrix - dimension: n x m
+        :param W: weights matrix - dimension: current_layer_features x prev_layer_features
+        :param C: classes vector matrix - dimension: l x m
+        :param b: bias vector - length: l"""
+        X = np.array([[10, 5, 8, 6],
+                      [11, 3, 2, 5],
+                      [6, 1, 7, 4]])  # n=3,  m=4
+
+        C = np.array([[1, 0, 0, 1],
+                      [0, 1, 1, 0]])  # l=2, m=4
+
+        W_0 = np.array([[1, 3, 4],
+                        [2, 4, 6]])  # n=2, l=3
+
+        b = np.random.randn(2, 1)
+
+        u = np.random.randn(2, 4)
+
+        d = np.random.rand(len(X), len(X[0]))
+        d = d / linalg.norm(d)
+        d_f = d.flatten()
+
+        Fw = lambda x: np.inner(np.tanh(x).flatten(), u.flatten())
+        Fw_delta = lambda x, epsilon: Fw(x) + epsilon * np.matmul(d_f,
+                                                                  backward.jacobianTMV_grad_x(x, W_0, u).flatten()) + epsilon ** 2
+
+        res_sum1 = []
+        res_sum2 = []
+        eps_0 = 0.001
+        input_x = np.matmul(W_0, X) + b
+        for i in range(0, 10):
+            epsi = (0.5 ** i) * eps_0
+            sum1 = abs(Fw_delta(input_x, epsi) - Fw(input_x))
+            sum2 = abs(Fw_delta(input_x, epsi) - Fw(input_x) - epsi * np.matmul(d_f,
+                                                                  backward.jacobianTMV_grad_x(input_x, W_0, u).flatten()))
+            res_sum1.append(sum1)
+            res_sum2.append(sum2)
+
+        factors_2 = [res_sum1[i] / res_sum1[i + 1] for i in range(0, len(res_sum1) - 1)]
+        factors_4 = [res_sum2[i] / res_sum2[i + 1] for i in range(0, len(res_sum2) - 1)]
+        print(f'factors 2: {factors_2}')
+        print(f'factors 4: {factors_4}')
+
+
 
     def test_a(self):
         """
@@ -118,5 +168,3 @@ class GradTests(unittest.TestCase):
         return 1 / (1 + np.exp(-x))
 
 
-class JacobianTests(unittest.TestCase):
-    pass
